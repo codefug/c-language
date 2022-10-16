3a2dd22284e74a0998ed4353997b956e6f53f6e2
codefug <86544979+codefug@users.noreply.github.com>
2022-10-17 03:55:21 +0900

    .

--- c language/중간고사_대표_출력_프로그램/대표프로그램.c
+++ c language/중간고사_대표_출력_프로그램/대표프로그램.c
@@ -1,57 +1,78 @@
 #include <stdio.h>
-
+#define rate 1120
 int main(void) {
 	int i = 1;
 
-	printf("%d장\n", i);
-	i += 1;
-	printf("Hello World!\n");
-	//hello world를 출력하라.
+	//printf("%d장\n", i);
+	//i += 1;
+	//printf("Hello World!\n");
+	////hello world를 출력하라.
 
 
-	printf("%d장\n", i);
-	i += 1;
-	int e = 1;
+	//printf("%d장\n", i);
+	//i += 1;
+	//int e = 1;
 
-	//구구단의 일부를 출력해보자
-	printf("3 X %d = %d\n", e, e * 3);
-	e += 1;
-	printf("3 X %d = %d\n", e, e * 3);
-	e += 1;
-	printf("3 X %d = %d\n", e, e * 3);
-	e += 1;
+	////구구단의 일부를 출력해보자
+	//printf("3 X %d = %d\n", e, e * 3);
+	//e += 1;
+	//printf("3 X %d = %d\n", e, e * 3);
+	//e += 1;
+	//printf("3 X %d = %d\n", e, e * 3);
+	//e += 1;
 
-	//사용자로부터 두 수를 받아서 더한 후에, 결과를 출력해보자.
-	int q;
-	int w;
-	printf("첫 번째 숫자를 입력하시오: ");
-	scanf("%d", &q);
-	printf("두 번째 숫자를 입력하시오: ");
-	scanf("%d", &w);
-	printf("두 수의 합 =%d", q + w);
+	////사용자로부터 두 수를 받아서 더한 후에, 결과를 출력해보자.
+	//int q;
+	//int w;
+	//printf("첫 번째 숫자를 입력하시오: ");
+	//scanf("%d", &q);
+	//printf("두 번째 숫자를 입력하시오: ");
+	//scanf("%d", &w);
+	//printf("두 수의 합 =%d\n", q + w);
 
-	//여행 비용을 계산해주는 프로그램을 작성해보자.
-	int day;
-	int price;
-	int allowance;
-	int hotel;
+	////여행 비용을 계산해주는 프로그램을 작성해보자.
+	//int day;
+	//int price;
+	//int allowance;
+	//int hotel;
 
-	printf("여행은 몇박인가요?: ");
-	scanf("%d", &day);
-	printf("항공권 가격: ");
-	scanf("%d", &price);
-	printf("호텔 1박 가격: ");
-	scanf("%d", &hotel);
-	printf("하루에 필요한 용돈: ");
-	scanf("%d", &allowance);
-	printf("====================\n");
-	printf("총 여행 비용:%d\n",day*(allowance+hotel)+price);
-	printf("====================\n");
+	//printf("여행은 몇박인가요?: ");
+	//scanf("%d", &day);
+	//printf("항공권 가격: ");
+	//scanf("%d", &price);
+	//printf("호텔 1박 가격: ");
+	//scanf("%d", &hotel);
+	//printf("하루에 필요한 용돈: ");
+	//scanf("%d", &allowance);
+	//printf("====================\n");
+	//printf("총 여행 비용:%d\n",day*(allowance+hotel)+price);
+	//printf("====================\n");
 
-	printf("%d장\n", i);
-	i += 1;
+	//printf("%d장\n", i);
+	//i += 1;
 
-	printf("%d장\n", i);
+	//int dollar;
+
+	//printf("달러화 금액을 입력하시오: ");
+	//scanf("%d", &dollar);
+	//printf("달러화 %d 달러는 %d입니다.", dollar * rate);
+
+	/*double speed = 300000.000000;
+	double distance = 149600000.000000;
+
+	printf("빛의 속도는 %lfkm/s\n",speed);
+	printf("태양과 지구와의 거리 %lfkm\n",distance);
+	printf("도달 시간은 %.6f분\n", distance / speed / 60);
+
+	double hwa;
+	double sup;
+
+	printf("화씨온도=");
+	scanf("%lf", &hwa);
+	sup = (hwa - 32) * 5 / 9;
+	printf("섭씨온도=%lf\n",sup);
+		*/
+	printf("%d장\n",i);
 	i += 1;
 
 	printf("%d장\n", i);
